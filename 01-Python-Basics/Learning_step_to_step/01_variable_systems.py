cf036b331fe16291736f6da3c1dc1670c8b1a257
Mustafa Zelzele <mustafasavas1300@gmail.com>
2026-01-03 16:33:20 +0300

    Completed Step 01: Variables and Data Types

---
+++ 01-Python-Basics/Learning_step_to_step/01_variable_systems.py
@@ -0,0 +1,14 @@
+# Step 1: Variables and Data Types
+user_name = "Mustafa"
+system_version = 1.2
+active_users_count = 121
+is_system_activate = True
+
+print(type(user_name))
+print(type(system_version))
+print(type(active_users_count))
+print(type(is_system_activate))
+
+# Casting float to string
+version_text = str(system_version)
+print("System Version: " + version_text)
