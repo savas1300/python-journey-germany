5cfdc7d1214e436b342e2aa179ee5ecdc23e68e0
Mustafa Zelzele <mustafasavas1300@gmail.com>
2026-01-06 14:56:27 +0300

    Completed Step 02: String manipulation and length check

---
+++ 01-Python-Basics/02_string_processing.py
@@ -0,0 +1,7 @@
+raw_string = " pyThon-is-AWESOME "
+new_raw_string = raw_string.strip().lower()
+cleaned_string = new_raw_string.replace("-" , " ")
+
+print(f"Original: '{raw_string}'")
+print(f"Cleaned: '{cleaned_string}'")
+print(f"Character Count: {len(cleaned_string)}") # Bu satırı ekleyebilirsin
